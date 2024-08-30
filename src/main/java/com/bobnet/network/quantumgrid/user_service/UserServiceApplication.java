6ef4e5892895b772a19e9cd6e44e36f4a17d06c2
Bobesz <bob@bobnet.network>
2024-08-30 11:30:34 +0200

    QUAN-28 Set up project structure using Spring Boot

---
+++ src/main/java/com/bobnet/network/quantumgrid/user_service/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.bobnet.network.quantumgrid.user_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class UserServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
